44bdd491def804024e462b476d6dd99a166736b3
Brian Cain <brianccain@gmail.com>
2016-01-12 19:31:24 -0800

    Generate player and enemy with GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,19 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
-  public Transform enemy;
+  public GameObject player;
+  public GameObject enemy;
 
   private int score;
   private List<Enemy> enemies;
+
+
+  void InitGame() {
+    Instantiate(player, new Vector3(0f, -3.5f, 0f), Quaternion.identity);
+    Instantiate(enemy, new Vector3(3.0f, 2.0f, 0f), Quaternion.identity);
+  }
+
+  void Awake() {
+    InitGame();
+  }
 }
