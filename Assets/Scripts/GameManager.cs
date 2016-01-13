86c3538518deeb349c8a787e3f2e392864b90609
Brian Cain <brianccain@gmail.com>
2016-01-12 19:52:57 -0800

    Generate list of enemies with random positions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,15 +6,26 @@ public class GameManager : MonoBehaviour {
 
   public GameObject player;
   public GameObject enemy;
+  public GameObject[] enemyArr;
   public GameObject meteor;
 
   private int score;
-  private List<Enemy> enemies;
-
+  private int enemyTotal;
 
   void InitGame() {
     Instantiate(player, new Vector3(0f, -3.5f, 0f), Quaternion.identity);
-    Instantiate(enemy, new Vector3(3.0f, 2.0f, 0f), Quaternion.identity);
+
+    enemyTotal = 3;
+    GenerateEnemyList(enemyTotal);
+  }
+
+  void GenerateEnemyList(int size) {
+    enemyArr = new GameObject[size];
+
+    for (int i = 0; i < size; i++) {
+      Vector3 randomPos = new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), 0f);
+      enemyArr[i] = (GameObject)Instantiate(enemy, randomPos, Quaternion.identity);
+    }
   }
 
   void Awake() {
